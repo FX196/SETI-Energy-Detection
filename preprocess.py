493622090b439349972791eff27d40d1b878e79c
fx196 <mikechen212@gmail.com>
2019-10-20 19:29:55 -0700

    globals

--- preprocess.py
+++ preprocess.py
@@ -11,6 +11,25 @@ from time import time
 
 from utils import *
 
+fil_path = "data/filterbanks/"
+h5_path = "data/h5/"
+
+test_fil = fil_path + "blc20_guppi_57991_48899_3C161_0007.gpuspec.0000.fil"
+
+fri_obs = h5_path + "GBT_57532_09539_HIP56445_fine.h5"
+
+plt_args = {
+            'aspect':'auto',
+            'origin':'lower',
+            'rasterized':True,
+            'interpolation':'nearest',
+            'cmap':'viridis'
+            }
+
+from dask.distributed import Client, progress
+client = Client(processes=False, threads_per_worker=3,
+                n_workers=4, memory_limit='4GB')
+
 def remove_broadband():
     pass
 
