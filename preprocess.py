fa6dd5137c3f05aae9669b5b035e143bdad7ac5b
fx196 <mikechen212@gmail.com>
2019-10-31 20:49:44 -0700

    update preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,18 @@
+import numpy as np
+from scipy import stats
+from blimpy import Waterfall
+from blimpy.utils import rebin
+from matplotlib import pyplot as plt
+from bisect import bisect_left
+from tqdm import tqdm
 import dask.array as da
 import h5py
+from time import time
+
+from utils import *
+import dask.array as da
+import h5py
+import sys
 
 fil_path = "data/filterbanks/"
 h5_path = "data/h5/"
@@ -16,13 +29,13 @@ plt_args = {
     'cmap': 'viridis'
 }
 
-from dask.distributed import Client
+# from dask.distributed import Client
+#
+# client = Client(processes=False, threads_per_worker=3,
+#                 n_workers=4, memory_limit='4GB')
 
-client = Client(processes=False, threads_per_worker=3,
-                n_workers=4, memory_limit='4GB')
 
-
-def to_npy_stack(source_h5_path, dest_path, channel_len=1033216):
+def to_npy_stack(source_h5_path, dest_path, verbose=False, channel_len=1033216):
     """
     Convert original h5 file to npy stack
 
@@ -31,9 +44,14 @@ def to_npy_stack(source_h5_path, dest_path, channel_len=1033216):
     :param channel_len:
     :return:
     """
+    if verbose:
+        start = time()
     h5_file = h5py.File(source_h5_path, "r")
     arr = da.from_array(h5_file["data"], chunks=(2, 1, channel_len * 14))
     da.to_npy_stack(dest_path, arr, axis=2)
+    if verbose:
+        end = time()
+        print(f"Converted to npy stack in {end - start} seconds.")
 
 
 def remove_broadband(source_h5_path):
@@ -45,3 +63,7 @@ def remove_bandpass():
 
 def gaussianity_thresholding():
     pass
+
+if __name__ == "__main__":
+    input_file, out_dir = sys.argv[1:3]
+    to_npy_stack(input_file, out_dir, True)
